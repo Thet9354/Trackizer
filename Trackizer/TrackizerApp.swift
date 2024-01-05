b34980aebdad4966e36b01698d7390449a7a4432
PhoonThetPine <thetpine254@gmail.com>
2024-01-05 17:55:53 +0800

    Initial Commit

---
+++ Trackizer/TrackizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackizerApp.swift
+//  Trackizer
+//
+//  Created by Phoon Thet Pine on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct TrackizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
